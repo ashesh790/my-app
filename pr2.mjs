a6ca527e601bd35faeaed3fb41f57edefc70d287
Ashesh <83585384+ashesh790@users.noreply.github.com>
2022-12-24 16:23:36 +0530

    Component-creation

---
+++ pr2.mjs
@@ -0,0 +1,11 @@
+let a = "Ashesh";
+let b = "udit";
+let c = "rohan";
+let d = "raj";
+let e = "deep"
+
+export default a;
+export { b };
+export { c };
+export { d };
+export { e };
---
+++ src/components/Cards.js
@@ -0,0 +1,103 @@
+import React from 'react'
+
+export default function Cards() {
+    return (
+        <div className='container container-fluid'>
+            <div className='row'>
+
+                <div className='col-sm-12 col-md-2 col-lg-2 col-xxl-3'>
+                    <div className='m-2'>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-sm-12 col-md-2 col-lg-2 col-xxl-3'>
+                    <div className='m-2'>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-sm-12 col-md-2 col-lg-2 col-xxl-3'>
+                    <div className='m-2'>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-sm-12 col-md-2 col-lg-2 col-xxl-3'>
+                    <div className='m-2'>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-sm-12 col-md-2 col-lg-2 col-xxl-3'>
+                    <div className='m-2'>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-sm-12 col-md-2 col-lg-2 col-xxl-3'>
+                    <div className='m-2'>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-sm-12 col-md-2 col-lg-2 col-xxl-3'>
+                    <div className='m-2'>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
